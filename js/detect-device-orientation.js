d3892fa712674f2e852b6fc2557f8c2b3af325cf
Karol Kowalczyk <KarolKowalczykPL@Gmail.com>
2024-02-05 20:29:01 +0100

    detect-device-orientation.js updated

--- js/detect-device-orientation.js
+++ js/detect-device-orientation.js
@@ -1,35 +1,36 @@
 window.addEventListener('resize', checkScreenWidth);
 
+const elements = [
+    'turn-device-img',
+    'turn-device-headline',
+    'start-screen',
+    'canvas-icons-div',
+    'phone-icons-div',
+    'start-button',
+    'headline'
+];
+
+function checkScreenOrientation() {
+    const portrait = window.matchMedia("(orientation: portrait)").matches;
+    return portrait;
+}
+
+function toggleElementVisibility(portrait, id) {
+    const element = document.getElementById(id);
+    if (portrait) {
+        element.classList.toggle('d-none', !id.includes('turn-device'));
+    } else {
+        element.classList.toggle('d-none', id.includes('turn-device'));
+    }
+}
+
+function toggleElementsVisibility(portrait) {
+    elements.forEach(id => {
+        toggleElementVisibility(portrait, id);
+    });
+}
 
 function checkScreenWidth() {
-
-    const portrait = window.matchMedia("(orientation: portrait)").matches;
-    let playIcons = document.getElementById('canvas-icons-div');
-    let startScreen = document.getElementById('start-screen');
-    let turnDeviceImg = document.getElementById('turn-device-img');
-    let turnDeviceHeadline = document.getElementById('turn-device-headline');
-    let phoneIconsDiv = document.getElementById('phone-icons-div');
-    let startButton = document.getElementById('start-button');
-    let headline = document.getElementById('headline');
-
-    if (portrait) {
-        turnDeviceImg.classList.remove('d-none');
-        turnDeviceHeadline.classList.add('turn-device-headline');
-        turnDeviceHeadline.classList.remove('d-none');
-        turnDeviceImg.classList.add('turn-device-img');
-        startScreen.classList.add('d-none');
-        playIcons.classList.add('d-none');
-        phoneIconsDiv.classList.add('d-none');
-        startButton.classList.add('d-none');
-        headline.classList.add('d-none');
-    } else {
-        turnDeviceHeadline.classList.remove('turn-device-headline');
-        turnDeviceHeadline.classList.add('d-none');
-        turnDeviceImg.classList.remove('turn-device-img');
-        startScreen.classList.remove('d-none');
-        playIcons.classList.remove('d-none');
-        phoneIconsDiv.classList.remove('d-none');
-        startButton.classList.remove('d-none');
-        headline.classList.remove('d-none');
-    }
+    const portrait = checkScreenOrientation();
+    toggleElementsVisibility(portrait);
 }
